0cbfb077769fb8c6ffa1e76e00bbb5f1a1e17982
MxSns <maxime.sn210@gmail.com>
2025-07-30 02:57:07 +0200

    update hashlib

--- passInput.py
+++ passInput.py
@@ -1,33 +1,34 @@
 # Script: WIP
 
 import getpass
+import hashlib
 
 # Variables
-name = input("Enter your name : ")
+name = input("Enter your username : ")
 password = None
 
 def password_set():
-    global password
-    password = getpass.getpass("Set a password: ")
-    first_check = input("Please confirm your password: ")
+    while True:
+        global password
+        password = getpass.getpass("Set a password: ")
+        first_check = getpass.getpass("Please confirm your password: ")
 
-    if first_check == password:
-        print("Password set")
-        return password        
-    else:
-        print("Passwords don't match")
-        password_set()
-
-password_set()
-
+        if first_check == password:
+            hashed_password = hashlib.sha256(password.encode()).hexdigest()
+            print("Password set")
+            return hashed_password        
+        else:
+            print("Passwords don't match")
+        
+password = password_set()
 user = [name, password]
 
 print(f"\nHello {name}")
 print(user)
 
-check = input("Please enter your password: ")
+check = getpass.getpass("Please enter your password: ")
 
-if user[1] == check:
+if hashlib.sha256(check.encode()).hexdigest() == user[1]:
     print("\nAccess granted")
 else:
     print("\nWrong password")
